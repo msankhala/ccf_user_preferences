daf4add31a4aa6b709bc2a9384b3bdc11340c5c0
nisubram <nisubram@estee.com>
2022-06-16 15:33:10 +0530

    CreatedApiForUserPreferences

--- src/Plugin/rest/resource/GetUserPreferences.php
+++ src/Plugin/rest/resource/GetUserPreferences.php
@@ -9,6 +9,7 @@ use Drupal\jwt\Transcoder\JwtTranscoderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\ccf_user_preferences\Entity\CcfUserPreferences;
 
 /**
  * Provides REST API for User preferences Details based
@@ -18,7 +19,8 @@ use Symfony\Component\HttpFoundation\Request;
  *   id = "get_user_preferences_rest_resource",
  *   label = @Translation("Get User Preferences"),
  *   uri_paths = {
- *     "canonical" = "/api/get_user_preferences"
+ *     "canonical" = "/api/get_user_preferences",
+ *     "create"    = "/api/add_user_preferences"
  *   }
  * )
  */
@@ -96,30 +98,94 @@ class GetUserPreferences extends ResourceBase
   {
     $data = [];
     $user_preferences = [];
-    if ($this->request->query->has('jwt_token'))
-    {
-      $jwt_token     = $this->request->query->get('jwt_token');
-      if (!empty($jwt_token)) {
-        $decoded_token = $this->transcoder->decode($jwt_token);
-        if(is_object($decoded_token)) {
-          $decoded_token_array =json_decode(json_encode($decoded_token->getPayload()),true);
-          $uid = $decoded_token_array['drupal']['uid'];
-          $uid = 4065;
-          if($uid) {
-            // Get user preferences_entity_id
-            $user = User::load($uid);
-            if($user) {
-              $user_preferences_id =  $user->get('field_user_preferences')->first();
-              $user_preferences    = $user_preferences_id->get('entity')->getTarget()->getValue();
-              foreach($this->mapper as $nestle_key => $drupal_field_name) {
-                $data['preferences'][$nestle_key] = array_column($user_preferences->get($drupal_field_name)->getValue(),'value');
+    try {
+      if ($this->request->query->has('jwt_token'))
+      {
+        $jwt_token     = $this->request->query->get('jwt_token');
+        if (!empty($jwt_token)) {
+          $decoded_token = $this->transcoder->decode($jwt_token);
+          if(is_object($decoded_token)) {
+            $decoded_token_array =json_decode(json_encode($decoded_token->getPayload()),true);
+            $uid = $decoded_token_array['drupal']['uid'];
+            $uid = 4041;
+            if($uid) {
+              // Get user preferences_entity_id
+              $user = User::load($uid);
+              if($user) {
+                $user_preferences_id =  $user->get('field_user_preferences')->first();
+                if($user_preferences_id) {
+                  $user_preferences    = $user_preferences_id->get('entity')->getTarget()->getValue();
+                  foreach($this->mapper as $nestle_key => $drupal_field_name) {
+                    $data['preferences'][$nestle_key] = array_column($user_preferences->get($drupal_field_name)->getValue(),'value');
+                  }
+                }
               }
             }
           }
         }
       }
     }
+    catch(\Exception $e) {
+      $data['error'] = $e->getMessage();
+    }
     return new ModifiedResourceResponse($data);
+  }
+
+  /**
+   * Responds to entity POST requests.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   Returning rest resource.
+   */
+  public function post($data)
+  {
+    $ccf = [];
+    try {
+      if(isset($data)) {
+        $jwt_token = $data['jwt_token'];
+        if (!empty($jwt_token)) {
+          $decoded_token = $this->transcoder->decode($jwt_token);
+          if(is_object($decoded_token)) {
+            $decoded_token_array =json_decode(json_encode($decoded_token->getPayload()),true);
+            $uid = $decoded_token_array['drupal']['uid'];
+            $user_preferences = $data['preferences'];
+            $uid = 4041;
+            $user = User::load($uid);
+            if($user) {
+              $user_preferences_entity =  $user->get('field_user_preferences')->first();
+              if($user_preferences_entity) {
+                $user_preferences_id = $user_preferences_entity->getValue()['target_id'];
+                $entity_update = CcfUserPreferences::load($user_preferences_id);
+                foreach($this->mapper as $nestle_key => $drupal_field_name) {
+                  if(!empty($user_preferences[$nestle_key])) {
+                    $entity_update->set($drupal_field_name,$user_preferences[$nestle_key]);
+                  }
+                }
+                $entity_update->save();
+              }
+              else {
+                foreach($this->mapper as $nestle_key => $drupal_field_name) {
+                  $ccf[$drupal_field_name] = $user_preferences[$nestle_key];
+                }
+                $ccf['type'] = 'ccf_user_preferences';
+                $ccf['uid'] = 1;
+                $ccf['status'] = 1;
+                $entity_creation = CcfUserPreferences::create($ccf);
+                $entity_creation->save();
+                $user->set('field_user_preferences',$entity_creation->id());
+                $user->save();
+              }
+            }
+            $message['message'] = "Successfully Updated";
+            return new ModifiedResourceResponse($message);
+          }
+        }
+      }
+    }
+    catch(\Exception $e) {
+      $message['error'] = $e->getMessage();
+      return new ModifiedResourceResponse($message);
+    }
   }
 }
 
